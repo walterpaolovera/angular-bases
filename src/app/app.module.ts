ce451b36e83c43936440dd3e7e8b42efb0ab538e
Walter Vera A. <vera.walter@gmail.com>
2024-07-24 19:10:00 -0500

    Fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from './counter/components/counter.module';
+import { HeroesModule } from './heroes/components/heroes.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { CounterComponent } from './counter/counter.component';
+// import { HeroComponent } from './heroes/hero/hero.component';
+// import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // CounterComponent,
+    // HeroComponent,
+    // ListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CounterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
